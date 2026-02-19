8e89179b4b2f6a949e5d231d9d3d32583e5058fa
Manish Patil <manish.iwdev@gmail.com>
2026-02-19 14:10:06 +0530

    Added

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './', // ← ADD THIS LINE
+  base: '/',
   server: {
     host: "::",
     port: 8080,
